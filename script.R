bdd6bff65e0ee2fcd402254cacb53e1b6a04d036
Diego Barneche <diego.barneche@ymail.com>
2019-02-26 10:55:31 +0100

    Added a useless comment

--- script.R
+++ script.R
@@ -2,6 +2,7 @@
 # FUNCTIONS #
 #############
 rescale  <-  function (x, range) {
+    # some useless comment
     p  <-  (x - min(x)) / (max(x) - min(x))
     range[[1]] + p * (range[[2]] - range[[1]])
 }
